e7a37b1f02ea7624b495693b7b3d844a6b407b32
joa-jo <joa@foxxing.com>
2021-07-15 17:28:52 +0900

    [COTS-3423] feat: firebase service import & authentification 연결

---
+++ src/service/cocktail_repo.js
@@ -0,0 +1,25 @@
+import { firebaseDatabase } from './firebase.js'
+
+class CocktailRepo {
+  readCocktails(userId, updateCocktailList) {
+    const ref = firebaseDatabase.ref(`${userId}/cocktails`)
+    ref.on('value', snapshot => {
+      const data = snapshot.val()
+      data && updateCocktailList(data)
+    })
+
+    return () => {
+      ref.off()
+    }
+  }
+
+  saveCocktail(userId, cocktail) {
+    firebaseDatabase.ref(`${userId}/cocktails/${cocktail.id}`).set(cocktail)
+  }
+
+  removeCocktail(userId, cocktail) {
+    firebaseDatabase.ref(`${userId}/cocktails/${cocktail.id}`).remove()
+  }
+}
+
+export default new CocktailRepo()
